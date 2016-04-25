8f70b3367cb94600dde6f2deefb5f1bc54d44865
Michael Daffin <michael.daffin@switchconcepts.com>
2016-04-25 13:19:11 +0100

    Adds more tests

--- src/lib.rs
+++ src/lib.rs
@@ -16,26 +16,29 @@ pub struct LoopControl {
 pub fn open_loop_control(dev: &str) -> Result<LoopControl, String> {
     let fd: c_int;
     let loctl = std::ffi::CString::new(dev).unwrap();
-
     unsafe {
         fd = open(loctl.as_ptr(), O_RDWR);
-        if fd < 0 {
-            return Err(format!("{}: {}", dev, errno::errno()));
-        }
-    };
-    Ok(LoopControl { fd: fd })
+    }
+
+    if fd < 0 {
+        Err(format!("{}: {}", dev, errno::errno()))
+    } else {
+        Ok(LoopControl { fd: fd })
+    }
 }
 
 impl LoopControl {
     pub fn next_free(&self) -> Result<String, String> {
+        assert!(self.fd >= 0);
         let result: i32;
         unsafe {
             result = ioctl(self.fd, 0x4C82);
         }
         if result < 0 {
-            panic!("result < 0");
+            Err(String::from(format!("{}", errno::errno())))
+        } else {
+            Ok(format!("{}{}", LOOP_PREFIX, result))
         }
-        Ok(format!("{}{}", LOOP_PREFIX, result))
     }
 }
 
@@ -91,14 +94,6 @@ mod tests {
     use libc;
 
     macro_rules! lc_open {
-        ( $name:ident, $x:expr ) => {
-            #[test]
-            fn $name() {
-                libc::set_return_value($x);
-                assert_eq!(open_loop_control("/dev/loop-control").unwrap(),
-                           LoopControl { fd: $x });
-            }
-        };
         ( $name:ident, $x:expr, $e:expr ) => {
             #[test]
             fn $name() {
@@ -108,18 +103,42 @@ mod tests {
         };
     }
 
-    lc_open!(lc_open_0, 0);
-    lc_open!(lc_open_1, 1);
-    lc_open!(lc_open_2, 2);
-    lc_open!(lc_open_100, 100);
-    lc_open!(lc_open_large, 1024 * 1024 * 1024);
+    lc_open!(lc_open_0, 0, Ok(LoopControl { fd: 0 }));
+    lc_open!(lc_open_1, 1, Ok(LoopControl { fd: 1 }));
+    lc_open!(lc_open_2, 2, Ok(LoopControl { fd: 2 }));
+    lc_open!(lc_open_100, 100, Ok(LoopControl { fd: 100 }));
+    lc_open!(lc_open_large, 1024 * 1024, Ok(LoopControl { fd: 1024 * 1024 }));
     lc_open!(lc_open_err,
              -1,
              Err(String::from("/dev/loop-control: Operation not permitted")));
 
+    macro_rules! lc_next_free {
+        ( $name:ident, $inp:expr, $out:expr, $exp:expr ) => {
+            #[test]
+            fn $name() {
+                libc::set_return_value($out);
+                let lc = LoopControl { fd: $inp };
+                assert_eq!(lc.next_free(), $exp);
+            }
+        };
+    }
+
+    lc_next_free!(lc_next_free_0, 0, 0, Ok(String::from("/dev/loop0")));
+    lc_next_free!(lc_next_free_1, 1, 1, Ok(String::from("/dev/loop1")));
+    lc_next_free!(lc_next_free_2, 2, 2, Ok(String::from("/dev/loop2")));
+    lc_next_free!(lc_next_free_5, 5, 5, Ok(String::from("/dev/loop5")));
+    lc_next_free!(lc_next_free_10, 10, 10, Ok(String::from("/dev/loop10")));
+    lc_next_free!(lc_next_free_54, 54, 54, Ok(String::from("/dev/loop54")));
+    lc_next_free!(lc_next_free_128, 128, 128, Ok(String::from("/dev/loop128")));
+    lc_next_free!(lc_next_free_err_1, 123, -1, Err(String::from("Operation not permitted")));
+    lc_next_free!(lc_next_free_err_2, 123, -2, Err(String::from("No such file or directory")));
+    lc_next_free!(lc_next_free_err_3, 123, -3, Err(String::from("No such process")));
     #[test]
-    fn lc_next_free() {
-        let lc = LoopControl { fd: 2 };
-        println!("{:?}", lc.next_free().unwrap());
+    #[should_panic(expected = "assertion failed")]
+    #[allow(unused_must_use)]
+    fn ln_next_free_panic() {
+        libc::set_return_value(-1);
+        let lc = LoopControl { fd: -1 };
+        lc.next_free();
     }
 }
