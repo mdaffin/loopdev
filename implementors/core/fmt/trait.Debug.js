682914e8b88ce1de4585f0d18a782c0f5a1e6135
Travis CI User <travis@example.org>
2017-12-04 17:24:35 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["loopdev"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"loopdev/struct.LoopControl.html\" title=\"struct loopdev::LoopControl\">LoopControl</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"loopdev/struct.LoopDevice.html\" title=\"struct loopdev::LoopDevice\">LoopDevice</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ loopdev/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["LoopControl","Interface to the loop control device: `/dev/loop-control`."],["LoopDevice","Interface to a loop device ie `/dev/loop0`."]]});
